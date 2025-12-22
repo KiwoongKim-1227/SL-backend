e3e440b2c1a96f64ef74459f3fa8df9747110541
nmmn1004 <jjj4120@gmail.com>
2025-12-22 15:47:44 +0900

    Fix: Modify the function of updatePinned in communityService defined value to toggle

--- src/main/java/com/gdg/slbackend/service/communityMembership/CommunityMembershipUpdater.java
+++ src/main/java/com/gdg/slbackend/service/communityMembership/CommunityMembershipUpdater.java
@@ -23,6 +23,6 @@ public class CommunityMembershipUpdater {
     }
 
     public void updatePinned(CommunityMembership communityMembership) {
-        communityMembership.updateIsPinned(true);
+        communityMembership.updateIsPinned(!communityMembership.isPinned());
     }
 }
