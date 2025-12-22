9ac60b6ec4984bd22d1aff1ca454e24e87b6dd46
nmmn <jjj4120@gmail.com>
2025-12-23 08:41:50 +0900

    Update CommunityMembershipRepository.java

--- src/main/java/com/gdg/slbackend/domain/community/CommunityMembershipRepository.java
+++ src/main/java/com/gdg/slbackend/domain/community/CommunityMembershipRepository.java
@@ -9,11 +9,22 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CommunityMembershipRepository extends JpaRepository<CommunityMembership, Long> {
-    Optional<CommunityMembership> findByCommunityIdAndUserId(Long communityId, Long userId);
-    List<CommunityMembership> findByCommunityOrderByIsPinnedDescIdAsc(Community community);
-    List<CommunityMembership> findAllByUserId(Long userId);
-    boolean existsByCommunityIdAndUserIdAndRole(Long communityId, Long userId, Role role);
+public interface CommunityMembershipRepository
+        extends JpaRepository<CommunityMembership, Long> {
+    Optional<CommunityMembership> findByCommunityIdAndUserId(
+            Long communityId,
+            Long userId
+    );
 
-    Long user(User user);
+    boolean existsByCommunityIdAndUserIdAndRole(
+            Long communityId,
+            Long userId,
+            Role role
+    );
+
+    List<CommunityMembership> findAllByUserId(Long userId);
+
+    List<CommunityMembership> findByCommunityOrderByIsPinnedDescIdAsc(
+            Community community
+    );
 }
